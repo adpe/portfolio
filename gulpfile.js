330983efe53b4d57bffcf8396ca5e276079ce499
Adrian Perez <me@adrianperez.me>
2023-04-12 21:52:02 +0200

    Sync theme and update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,16 @@ const cssnano = require('cssnano');
 const rename = require('gulp-rename');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function () {
     return gulp.src([
             'themes/resume/static/css/*.css',
             '!themes/resume/static/css/*min.css'
         ])
+        .pipe(sourcemaps.init())
         .pipe(postcss([autoprefixer, cssnano]))
+        .pipe(sourcemaps.write('.'))
         .pipe(rename({
             suffix: '.min'
         }))
