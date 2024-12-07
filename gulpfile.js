30e49ba2f9b0905e80fbf4a326ad567a9d00d7f1
Adrian Perez <me@adrianperez.me>
2024-12-07 14:12:47 +0100

    Improve Gulpfile and minify JS and concat CSS/JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,43 @@
 const gulp = require('gulp');
 const cssnano = require('cssnano');
-const rename = require('gulp-rename');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const gulpIf = require('gulp-if');
+const uglify = require('gulp-uglify');
 
-gulp.task('default', function () {
+function isNotMinified(file) {
+    return !file.path.endsWith('.min.css') && !file.path.endsWith('.min.js');
+}
+
+gulp.task('minify-css', function () {
     return gulp.src([
+            'themes/resume/static/css/libs/bootstrap.min.css',
+            'themes/resume/static/css/libs/fork-awesome.min.css',
+            'themes/resume/static/css/libs/icomoon.min.css',
+            'themes/resume/static/css/libs/simple-line-icons.min.css',
             'themes/resume/static/css/*.css',
-            '!themes/resume/static/css/*min.css'
         ])
         .pipe(sourcemaps.init())
-        .pipe(postcss([autoprefixer, cssnano]))
+        .pipe(gulpIf(isNotMinified, postcss([autoprefixer, cssnano])))
+        .pipe(concat('all.min.css'))
         .pipe(sourcemaps.write('.'))
-        .pipe(rename({
-            suffix: '.min'
-        }))
         .pipe(gulp.dest('themes/resume/static/css'));
 });
+
+gulp.task('minify-js', function () {
+    return gulp.src([
+            'themes/resume/static/js/libs/jquery.min.js',
+            'themes/resume/static/js/libs/bootstrap.bundle.min.js',
+            'themes/resume/static/js/libs/jquery.easing.min.js',
+            'themes/resume/static/js/*.js',
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(gulpIf(isNotMinified, uglify()))
+        .pipe(concat('all.min.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('themes/resume/static/js'));
+});
+
+gulp.task('default', gulp.parallel('minify-css', 'minify-js'));
