2a12f65d892b9b43e460c45a566def24f5d86ec9
Adrian Perez <me@adrianperez.me>
2026-02-15 19:43:46 +0100

    Fix tags in layout and add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,18 @@ import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   site: "https://adrianperez.me/",
   integrations: [mdx(), sitemap(), react(), markdoc(), keystatic()],
   output: "static",
+
   vite: {
     ssr: {
       external: ["svgo"],
     },
   },
+
+  adapter: netlify(),
 });
