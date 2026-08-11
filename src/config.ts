b7d958dce8ee8b10d5c1badd2c6baba5a7bd9a1b
Adrian Perez <me@adrianperez.me>
2026-08-11 23:12:17 +0200

    Update job description

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,8 @@ export const siteConfig = {
   contactNote: "Digital Native and Webenthusiast",
   profileImage: "/img/me.jpg",
   email: "hi@adrianperez.me",
-  description: "I am a CRM developer in the Web Development Services department at AMAG Corporate Services AG, currently live in Fürigen, Switzerland. My interests range from programming to travel. I am also interested in DIY, sports, and technology.",
+  description:
+    "I am an API Platform Engineer at PostFinance AG currently living in Fürigen, Switzerland. My interests range from programming to travel. I am also interested in DIY, sports, and technology.",
   favicon: "favicon.ico",
   showQr: false,
   showSkills: true,
@@ -21,37 +22,37 @@ export const siteConfig = {
     {
       name: "LinkedIn",
       link: "https://www.linkedin.com/in/adpe",
-      icon: "linkedin"
+      icon: "linkedin",
     },
     {
       name: "Xing",
       link: "https://www.xing.com/profile/Adrian_Perez24/cv",
-      icon: "xing"
+      icon: "xing",
     },
     {
       name: "GitHub",
       link: "https://github.com/adpe/",
-      icon: "github"
+      icon: "github",
     },
     {
       name: "Stack Overflow",
       link: "https://stackoverflow.com/story/adpe",
-      icon: "stack-overflow"
+      icon: "stack-overflow",
     },
     {
       name: "Telegram",
       link: "https://t.me/adpe90",
-      icon: "telegram"
+      icon: "telegram",
     },
     {
       name: "Signal",
       link: "https://signal.group/#CjQKIPTT3JKSxGiXBnhneQDkZl1AWSLtKZrtKeRPol2UnNZyEhA2zEqeG3rdV8iaLwtKkr4U",
-      icon: "signalapp"
+      icon: "signalapp",
     },
     {
       name: "FindPenguins",
       link: "https://findpenguins.com/adrianperez",
-      icon: "icomoon-findpenguins"
-    }
-  ]
+      icon: "icomoon-findpenguins",
+    },
+  ],
 };
